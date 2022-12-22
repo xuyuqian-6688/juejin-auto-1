c1719cc94bd0258bf51daa4f146f1b0e42736999
宋迎春 <songyingchun@harsons.cn>
2022-12-22 16:49:10 +0800

    feat: 更新配置注释

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,9 @@
-/**
- * Created by huangqihong on 2022/1/8.
+/*
+ * @Author: songyingchun
+ * @Date: 2022-04-14 18:05:16
+ * @Description: 配置参数
  */
+
 module.exports = {
   COOKIE: process.env.COOKIE || '',
   TOKEN: process.env.TOKEN || '',
